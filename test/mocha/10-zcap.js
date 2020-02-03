f58523a96dbafc8de41c18fb8c2bfa19bdaedc51
Matthew Collier <mcollier@digitalbazaar.com>
2020-02-03 12:52:18 -0500

    Implement revocations APIs.

---
+++ test/mocha/10-zcap.js
@@ -0,0 +1,18 @@
+/*!
+ * Copyright (c) 2019-2020 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const {config, util: {clone}} = require('bedrock');
+const brZcapStorage = require('bedrock-zcap-storage');
+const database = require('bedrock-mongodb');
+const mockData = require('./mock-data');
+
+describe('zcap API', () => {
+  describe('insert API', () => {
+    it('should work');
+  });
+  describe('get API', () => {
+    it('should work');
+  });
+});
---
+++ test/mocha/mock-data.js
@@ -0,0 +1,17 @@
+/*!
+ * Copyright (c) 2019-2020 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const mocks = {};
+module.exports = mocks;
+
+const actors = mocks.actors = {};
+
+actors.alpha = {
+  id: 'urn:uuid:ec6bcc36-e7ab-46e9-aebb-ab57caee4fbe'
+};
+
+actors.beta = {
+  id: 'urn:uuid:df29c22a-9c68-448c-a04b-345383aaf8ff'
+};
