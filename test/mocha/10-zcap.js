dba91cf23c90c66b09d385eb1e7b49792475ca61
Andrew L Jones <ajones@digitalbazaar.com>
2020-06-29 16:01:46 -0400

    Add get tests.

--- test/mocha/10-zcap.js
+++ test/mocha/10-zcap.js
@@ -3,9 +3,11 @@
  */
 'use strict';
 
-const {config, util: {clone}} = require('bedrock');
+const {config, util: {uuid}} = require('bedrock');
 const {httpsAgent} = require('bedrock-https-agent');
 const {create} = require('apisauce');
+const {zcap, actors} = require('./mock-data');
+const {stubPassport} = require('./helpers');
 
 const cfg = config['zcap-storage-http'];
 const {routes} = cfg;
@@ -16,15 +18,64 @@ const api = create({
   timeout: 1000
 });
 
+const actor = actors.alpha;
+
 describe('capabilties API', function() {
+  let capability, stubs = null;
+  before(function() {
+    stubs = stubPassport({actor});
+  });
+  beforeEach(function() {
+    capability = zcap();
+  });
+  after(function() {
+    stubs.optionallyAuthenticated.restore();
+    stubs.ensureAuthenticated.restore();
+  });
   describe('insert API', function() {
     it('should insert a capability', async function() {
-
+      capability.controller = actor.id;
+      const result = await api.post('', {controller: actor.id, capability});
+      should.exist(result);
+      result.status.should.equal(204);
     });
+    it('should not insert a capability if the actor is not the controller',
+      async function() {
+        const result = await api.post(
+          '', {controller: 'did:key:non-controller', capability});
+        should.exist(result);
+        result.status.should.equal(400);
+      });
   });
   describe('get API', function() {
-    it('should work', async function() {
-
+    it('should get a zcap by id', async function() {
+      capability.controller = actor.id;
+      const id = `urn:zcap:${uuid()}`;
+      capability.id = id;
+      capability.referenceId = id;
+      const insertResult = await api.post(
+        '', {controller: actor.id, capability});
+      should.exist(insertResult);
+      insertResult.status.should.equal(204);
+      const getResult = await api.get('', {controller: actor.id, id});
+      should.exist(getResult);
+      getResult.status.should.equal(200);
+      getResult.data.should.deep.equal(capability);
+    });
+    it('should get a zcap by referenceId', async function() {
+      capability.controller = actor.id;
+      const id = `urn:zcap:${uuid()}`;
+      const referenceId = 'test-ref-id';
+      capability.id = id;
+      capability.referenceId = referenceId;
+      const insertResult = await api.post(
+        '', {controller: actor.id, capability});
+      should.exist(insertResult);
+      insertResult.status.should.equal(204);
+      const getResult = await api.get('', {controller: actor.id, referenceId});
+      should.exist(getResult);
+      getResult.status.should.equal(200);
+      getResult.data.should.deep.equal(capability);
     });
   });
   describe('delete API', function() {
