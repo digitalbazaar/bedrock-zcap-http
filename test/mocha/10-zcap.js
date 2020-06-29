6b37ead61468ea11e44ab38752d22825458bbb16
Andrew L Jones <ajones@digitalbazaar.com>
2020-06-29 14:11:13 -0400

    Layout capabilities tests.

--- test/mocha/10-zcap.js
+++ test/mocha/10-zcap.js
@@ -3,16 +3,33 @@
  */
 'use strict';
 
-// const {config, util: {clone}} = require('bedrock');
-// const brZcapStorage = require('bedrock-zcap-storage');
-// const database = require('bedrock-mongodb');
-// const mockData = require('./mock-data');
+const {config, util: {clone}} = require('bedrock');
+const {httpsAgent} = require('bedrock-https-agent');
+const {create} = require('apisauce');
 
-describe('zcap API', () => {
-  describe('insert API', () => {
-    it('should work');
+const cfg = config['zcap-storage-http'];
+const {routes} = cfg;
+
+const api = create({
+  baseURL: `${config.server.baseUri}${routes.capabilities}`,
+  httpsAgent,
+  timeout: 1000
+});
+
+describe('capabilties API', function() {
+  describe('insert API', function() {
+    it('should insert a capability', async function() {
+
+    });
   });
-  describe('get API', () => {
-    it('should work');
+  describe('get API', function() {
+    it('should work', async function() {
+
+    });
+  });
+  describe('delete API', function() {
+    it('should work', async function() {
+
+    });
   });
 });
